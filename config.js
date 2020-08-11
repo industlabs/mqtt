f1cad9ba99a143141b17e28612320a27d8a72234
industlabs <57274248+industlabs@users.noreply.github.com>
2020-08-10 23:48:28 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 // host = '172.16.153.122';	// hostname or IP address
-host = '127.0.0.1';	// hostname or IP address
+host = 'mqtt.casi.io';	// hostname or IP address
 // host = '172.16.153.110';	// hostname or IP address
-port = 9001;
+port = 8084;
 topic = '#';		// topic to subscribe to
 useTLS = false;
-username = null;
-password = null;
-// username = "jjolie";
-// password = "aa";
+//username = null;
+//password = null;
+username = "admin";
+password = "4887_Alpha";
 
 // path as in "scheme:[//[user:password@]host[:port]][/]path[?query][#fragment]"
 //    defaults to "/mqtt"
